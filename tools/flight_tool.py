49a75f199cbb8f2e2be0b221f9d1e10bd9cbc049
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-05-31 15:21:09 +0530

    Update flight_tool.py

--- tools/flight_tool.py
+++ tools/flight_tool.py
@@ -53,11 +53,11 @@ def search_flights(query: str) -> str:
 
     result = (
         f"✈️ Cheapest Flight from {from_city} to {to_city} \n\n"
-        f" Flight Date : {Date}\n"end=''
+        f" Flight Date : {Date}\n\n"
         f"  Flight ID: {cheapest_flight['flight_id']}\n\n"
         f"  Airline: {cheapest_flight['airline']}\n\n"
         f"  Departure: {formatted_departure_time}\n\n"
-        f"  Arrival: {formatted_arrival_time}\n"
+        f"  Arrival: {formatted_arrival_time}\n\n"
         f"  Price: ₹{cheapest_flight['price']:,.0f}"
        
     )
