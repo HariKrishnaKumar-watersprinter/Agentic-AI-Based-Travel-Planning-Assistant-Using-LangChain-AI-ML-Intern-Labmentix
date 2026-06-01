bcf9a9032365e5eafba3afb1f0e5861e136350b3
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-06-01 09:52:24 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -94,9 +94,9 @@ def build_agent(verbose: bool = True) -> AgentExecutor:
         AgentExecutor ready to process travel queries.
     """
     # Initialize the Google Gemini LLM
-    api_key3 = st.secrets[api_key3]
-    api_key = st.secrets[api_key]
-    api_key2 = st.secrets[api_key2]
+    api_key3 = st.secrets['api_key3']
+    api_key = st.secrets['api_key']
+    api_key2 = st.secrets['api_key2']
     
     llm = ChatGoogleGenerativeAI(api_key=api_key3,model="gemini-2.5-flash")
 
